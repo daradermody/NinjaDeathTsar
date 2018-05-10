525c88aa86712e9b80d1542a28dc9915a5a76152
Dara Dermody <daradermody@gmail.com>
2018-05-10 22:24:46 +0100

    Rotation component removed

---
+++ Assets/OrthographicResizer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class OrthographicResizer : MonoBehaviour {
+    public Camera orthographicCamera;
+    public int pixelsPerUnit;
+    public int minOrthographicSize = 6;
+
+    int verticalScreenSize = 0;
+    int currentScreenSize;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentScreenSize = Screen.height;
+        if (verticalScreenSize != currentScreenSize) {
+            verticalScreenSize = currentScreenSize;
+
+            int monitorPixelsPerSpritePixel = (int)((float)verticalScreenSize / minOrthographicSize / 2 / pixelsPerUnit);
+            float orthographicSize = (float)verticalScreenSize / (pixelsPerUnit * monitorPixelsPerSpritePixel) / 2;
+            orthographicCamera.orthographicSize = orthographicSize;
+        }
+		
+	}
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,48 +8,30 @@ public class PlayerController : MonoBehaviour {
 
     public float jumpCoefficient;
     public float movementCoefficient;
-    public float rotationCoefficient;
-    public int maxVelocity;
 
-    public Transform left_foot;
-    public Transform right_foot;
+    public Transform bottom;
     public LayerMask ground_layers;
 
-    Vector2 gravity;
-
-    // Use this for initialization
     void Start() {
         playerController = GetComponent<Rigidbody2D>();
-        gravity = Physics2D.gravity;
-    }
-
-    // Update is called once per frame
-    void Update() {
     }
 
     private void FixedUpdate() {
+        ControlMovement();
+        ControlJump();
+    }
 
-
+    private void ControlMovement() {
         float h = Input.GetAxis("Horizontal");
+        playerController.velocity = new Vector2(h * movementCoefficient * 10, playerController.velocity.y);
+    }
 
-        bool grounded = Physics2D.OverlapArea(left_foot.position, right_foot.position, ground_layers);
-        print(grounded);
-        if (!grounded || Math.Abs(playerController.rotation) < 5) {
-            playerController.velocity = new Vector2(h * movementCoefficient * 10, playerController.velocity.y);
-        } else {
-            playerController.velocity = Quaternion.AngleAxis(playerController.rotation, Vector3.forward) * new Vector2(h * movementCoefficient * 10, playerController.velocity.y);
-        }
-        
+    private bool IsGrounded() {
+        return Physics2D.OverlapPoint(bottom.position, ground_layers);
+    }
 
-        if (grounded) {
-            //Physics2D.gravity = Vector2.zero;
-            if (Input.GetButton("Jump"))
-                playerController.AddForce(Vector2.up * jumpCoefficient * 100);
-        }// else if (Physics2D.gravity == Vector2.zero)
-          //  Physics2D.gravity = gravity;
-
-
-        float rotation = Input.GetAxis("Mouse ScrollWheel");
-        playerController.AddTorque(rotation * rotationCoefficient * 10);
+    private void ControlJump() {
+        if (Input.GetButton("Jump") && IsGrounded())
+            playerController.AddForce(Vector2.up * jumpCoefficient * 100);
     }
 }
